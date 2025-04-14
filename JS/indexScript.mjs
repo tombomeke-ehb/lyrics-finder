491ba8436f7552bd6c5fc3f6fddb97ab9fb23c46
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-04-14 02:06:22 +0200

    Redoing CSS on second page

---
+++ JS/indexScript.mjs
@@ -0,0 +1,7 @@
+const loadingOverlay = document.getElementById('loadingOverlay');
+loadingOverlay.style.display = 'flex';
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    loadingOverlay.style.display = 'none';
+});
