d623c9697fe32c47a90d8326f71839728b2a276e
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-05-18 22:53:50 +0200

    Added more Utils
    I made it so that all the settings get imported after loading the page as some sort of double check

--- src/JS/themeUtils.mjs
+++ src/JS/themeUtils.mjs
@@ -7,3 +7,24 @@ export function applyThemeFromStorage() {
     document.body.classList.remove("light-mode");
   }
 }
+
+export function applyAnimationsFromStorage() {
+  const animSetting = localStorage.getItem("animations") || "on";
+  document.body.classList.toggle("no-animation", animSetting !== "on");
+}
+
+
+export function applySongLimitFromStorage() {
+  const songLimit = localStorage.getItem("songLimit") || "10";
+  const select = document.getElementById("song-limit");
+  if (select) {
+    select.value = songLimit;
+  }
+}
+
+
+export function applySettingsFromStorage() {
+  applyThemeFromStorage();
+  applyAnimationsFromStorage();
+  applySongLimitFromStorage();
+}
